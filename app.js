94da04ebad1cfb7b29cfc0cef658139bbb05a8c8
mbahgatTech <mazenbahgat@outlook.com>
2022-04-04 21:39:03 -0400

    Forecast now given based on the user's IP address

--- app.js
+++ app.js
@@ -23,14 +23,57 @@ app.get('/client-code',(req,res) => {
 
 // simple request for the forecast weather
 app.get('/forecast', (req, res) => {
+    let location = req.query;
+    console.log(location);
+
     // make axios call to OpenWeather API for our latitude and longitude
     // to get the forecast
-    axios({
-        method: 'get',
-        url: 'https://api.openweathermap.org/data/2.5/onecall?lat=43.544804&lon=-80.248169&appid=' + apiKey
-    }).then(function (response) {
-        res.send(response.data);
-    });
+    try {
+        axios({
+            method: 'get',
+            url: `https://api.openweathermap.org/data/2.5/onecall?lat=${location.lat}&lon=${location.lon}&units=metric&appid=` + apiKey
+        }).then(function (response) {
+            try {
+                let returnData = {};
+    
+                // create an array of days and include the relevant weather 
+                // informationfor of each day to our app 
+                returnData.daily = [];
+                for (let curr of response.data.daily) {
+                    let day = {};
+                    day.humidity = curr.humidity;
+                    day.max = curr.temp.max;
+                    day.min = curr.temp.min;
+                    day.snow = curr.snow;
+                    day.rain = curr.rain;
+                    day.weather = curr.weather;
+                    
+                    returnData.daily.push(day);
+                }
+                
+                // create a today element that has the weather information for the current day
+                returnData.current = {};
+                returnData.current.temp = response.data.current.temp;
+                returnData.current.feels_like = response.data.current.feels_like;
+                returnData.current.humidity = response.data.current.humidity;
+                returnData.current.sunrise = response.data.current.sunrise;
+                returnData.current.sunset = response.data.current.sunset;
+                returnData.current.clouds = response.data.current.clouds;
+                returnData.current.weather = response.data.current.weather;
+                
+                res.send(returnData);
+            }
+            catch(err) {
+                console.log(err);
+                res.send(err);
+            }
+    
+        });
+    }
+    catch (e) {
+        console.log(e);
+        res.send(e);
+    }
 });
-
+    
 app.listen(5000);
--- public/index.js
+++ public/index.js
@@ -1,15 +1,42 @@
 $(function() {
+    let location = {"lon":79.3871,"lat":43.6426};
+    let forecast = {};
+    
     $.ajax({
-        type: 'get',         
-        dataType: 'json',    
-        url: '/forecast',   
-        success: function (data) {
-            console.log(data);
-            console.log("Fetching forecast success.");
+        type: 'get',
+        dataType: 'json',
+        url: 'http://ip-api.com/json/',
+        async: false,
+        success: function(response) {
+            location = response;
+            getForecast(location);
         },
-        fail: function(error) {
-            alert('Failed to load the forecast.');
-            console.log(error); 
+        fail: function(data) {
+            getForecast(location);
+            console.log('Failed to fetch the user\'s location.');
         }
     });
+    
+    // once the forecast has been given, show the values
+    // in the gui
+    $(document).ajaxComplete(function (event, xhr, settings) {
+        console.log(forecast);
+    });
+
+    function getForecast(location) {
+        $.ajax({
+            type: 'get',         
+            dataType: 'json',    
+            url: '/forecast',   
+            data: location,
+            success: function (data) {
+                forecast = data;
+                console.log("Fetching forecast success.");
+            },
+            fail: function(error) {
+                alert('Failed to load the forecast.');
+                console.log(error); 
+            }
+        });
+    }
 });
